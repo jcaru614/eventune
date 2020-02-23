0dba437a9c12be1b73e52d2d257dbce5a352eb65
Joseph Caruana <jcaruana614@gmail.com>
2020-02-22 16:59:54 -0800

    git ignore and urls

--- eventune_app/views.py
+++ eventune_app/views.py
@@ -3,5 +3,8 @@ from django.contrib import messages
 from .models import *
 import bcrypt
 
+def index(request):
+    return render(request, 'index.html')
+
 def home(request):
     return render(request, 'home.html')
