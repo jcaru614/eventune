306e2315a75ead33da41f9a8d6f7563479808820
Joseph Caruana <jcaruana614@gmail.com>
2020-02-22 16:05:24 -0800

    homepage

--- eventune_app/views.py
+++ eventune_app/views.py
@@ -3,5 +3,5 @@ from django.contrib import messages
 from .models import *
 import bcrypt
 
-def index(request):
-    return render(request, 'index.html')
+def home(request):
+    return render(request, 'home.html')
