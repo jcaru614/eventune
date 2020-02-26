42d9d5b3b430443f16f60efb3e16671110695c8f
Joseph Caruana <jcaruana614@gmail.com>
2020-02-26 12:33:47 -0800

    adding event to user

--- eventune_app/static/js/index.js
+++ eventune_app/static/js/index.js
@@ -1,57 +1,61 @@
 $(document).ready(function () {
     var city = $('#city').val();
-   $.ajax({
-       type: "GET",
-       url: "https://app.ticketmaster.com/discovery/v2/events.json?size=42&city="+city+"&apikey=V39QrEGIiAS8Nin2Zy3pJU8nJBNoiFNm",
-       async: true,
-       dataType: "json",
-       success: function (json) {
-           console.log(json);
-           console.log(json._embedded.events)
-           var htmlString = ""
-           for (var i = 0; i < json._embedded.events.length; i++) {
-               var events = (json._embedded.events[i])
-               for (var j = 0; j < events.images.length; j++) {
-                   if (events.images[j]['width'] >= 1024)
-                       var img = events.images[j].url
-               }
-               console.log(events)
-               var imgTag = '<img src="' + img + '" class="card-img-top" />'
-               var eventName = '<p class="card-text">' + json._embedded.events[i].name + '</p>'
-               var eventDate = '<p class="card-text">' + json._embedded.events[i].dates.start['localDate'] + '</p>'
-               var moreInfo = '<a href="' + json._embedded.events[i].url + '" class="btn btn-info m-info"> More Info </a>'
-               var button = "<button type='button' class='btn btn-warning btn-interes'>Interested</button>"
-               htmlString += "<div class='card concerts'>"+imgTag+"<div class='card-body'>"+eventName+eventDate+moreInfo+button+"</div></div>"
-           }
-           console.log(htmlString)
-           $(".main-container").html(htmlString)
-       },
-       error: function (xhr, status, err) {
-       }
-   });
-   htmlString += "<div class='card concerts'> <img src='" + img + "' class='card-img-top' /> <div class='card-body'> <p class='card-text'>" + json._embedded.events[i].name + "</p> <p class='card-text'>" + json._embedded.events[i].dates.start['localDate'] + "</p> <a href='" + json._embedded.events[i].url + "' class='btn btn-info m-info'> More Info </a> <button type='button' class='btn btn-warning btn-interes'>Interested</button> </div> </div> "
+    $.ajax({
+        type: "GET",
+        url: "https://app.ticketmaster.com/discovery/v2/events.json?size=42&city=" + city + "&apikey=V39QrEGIiAS8Nin2Zy3pJU8nJBNoiFNm",
+        async: true,
+        dataType: "json",
+        success: function (json) {
+            console.log(json);
+            console.log(json._embedded.events)
+            var htmlString = ""
+            for (var i = 0; i < json._embedded.events.length; i++) {
+                var events = (json._embedded.events[i])
+                for (var j = 0; j < events.images.length; j++) {
+                    if (events.images[j]['width'] >= 1024)
+                        var img = events.images[j].url
+                }
+                console.log(events)
+                var imgTag = '<img src="' + img + '" class="card-img-top" />'
+                var eventName = '<p class="card-text">' + json._embedded.events[i].name + '</p>'
+                var eventDate = '<p class="card-text">' + json._embedded.events[i].dates.start['localDate'] + '</p>'
+                var moreInfo = '<a href="' + json._embedded.events[i].url + '" class="btn btn-info m-info"> More Info </a>'
+                var button = "<form action='/add_event{{user.id}}' method='POST'> <button type='submit' class='btn btn-warning btn-interes'>Interested</button>"
+                htmlString += "<div class='card concerts'>" + imgTag + "<div class='card-body'>" + eventName + eventDate + moreInfo + button + "</div></div>"
+            }
+            console.log(htmlString)
+            $(".main-container").html(htmlString)
+        },
+        error: function (xhr, status, err) {
+        }
+    });
 
-   //  search  // %20 for spaces in api url
+    //  search  // %20 for spaces in api url
 
-   $('form').submit(function (e) {
-       e.preventDefault();
-       var keyword = $('#searchparam').val(); //$('#keyword').value; // get keyword from form in var
-       console.log(keyword);
-       $.get("https://app.ticketmaster.com/discovery/v2/events.json?&keyword=" + keyword + "&apikey=V39QrEGIiAS8Nin2Zy3pJU8nJBNoiFNm",
-           function (json) {
-               console.log(json);
-               var htmlString = ""
-               for (var i = 0; i < json._embedded.events.length; i++) {
-                   var events = (json._embedded.events[i])
-                   for (var j = 0; j < events.images.length; j++) {
-                       if (events.images[j]['width'] >= 1024)
-                           var img = events.images[j].url
-                   }
-                   htmlString += "<div class='card concerts'> <img src='" + img + "' class='card-img-top' /> <div class='card-body'> <p class='card-text'>" + json._embedded.events[i].name + "</p> <p class='card-text'>" + json._embedded.events[i].dates.start['localDate'] + "</p> <a href='" + json._embedded.events[i].url + "' class='btn btn-info m-info'> More Info </a> <button type='button' class='btn btn btn-warning btn-interes'>Interested</button> </div> </div> "
-               }
-               $(".main-container").html(htmlString)
-           })
-   })
+    $('form').submit(function (e) {
+        e.preventDefault();
+        var keyword = $('#searchparam').val(); //$('#keyword').value; // get keyword from form in var
+        console.log(keyword);
+        $.get("https://app.ticketmaster.com/discovery/v2/events.json?&keyword=" + keyword + "&apikey=V39QrEGIiAS8Nin2Zy3pJU8nJBNoiFNm",
+            function (json) {
+                console.log(json);
+                var htmlString = ""
+                for (var i = 0; i < json._embedded.events.length; i++) {
+                    var events = (json._embedded.events[i])
+                    for (var j = 0; j < events.images.length; j++) {
+                        if (events.images[j]['width'] >= 1024)
+                            var img = events.images[j].url
+                    }
+                    var imgTag = '<img src="' + img + '" class="card-img-top" />'
+                    var eventName = '<p class="card-text">' + json._embedded.events[i].name + '</p>'
+                    var eventDate = '<p class="card-text">' + json._embedded.events[i].dates.start['localDate'] + '</p>'
+                    var moreInfo = '<a href="' + json._embedded.events[i].url + '" class="btn btn-info m-info"> More Info </a>'
+                    var button = "<form action='/add_event{{user.id}}' method='POST'> <button type='submit' class='btn btn-warning btn-interes'>Interested</button>"
+                    htmlString += "<div class='card concerts'>" + imgTag + "<div class='card-body'>" + eventName + eventDate + moreInfo + button + "</div></div>"
+                }
+                $(".main-container").html(htmlString)
+            })
+    })
 
 })
 
--- eventune_app/urls.py
+++ eventune_app/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('update/<int:id>', views.update),
     path('updatepw/<int:id>', views.update_pw),
     path('delete/<int:id>', views.delete_account),
+    path('add_event/<int:id>', views.add_event),
     path('myevents', views.my_events),
     path('logout', views.logout),
 ]
