16a41c41bd94c45dcc4a1eae865fd5aad45d7417
Camila <kmivivi12@gmail.com>
2020-06-17 13:13:35 -0700

    Updated settings

--- eventune/settings.py
+++ eventune/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '1fhu5u(=&2cn#s44)fh$$4@=nzb784p#+(fb7o-lp*el)hgsix'
+# SECRET_KEY = '1fhu5u(=&2cn#s44)fh$$4@=nzb784p#+(fb7o-lp*el)hgsix'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
